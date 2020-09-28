00b8626430a414d8a2b9dc16043d938fc6898cb9
Diego Cáceres <caceres123dfcc@gmail.com>
2020-09-27 21:53:16 -0600

    Tarea01 Diego Cáceres

--- Exercise01-Result.py
+++ Exercise01-Result.py
@@ -1,28 +1,31 @@
 # Creando lista vacia
-RegistroVentas =[]
+RegistroVentas = []
 
 # El usuario ingresa si quiere o no ingresar un cliente
 opcion = 0
+CostoTotal = 0
 opcion = input("¿Deseas ingresar un cliente?: ")
 
-#El usuario ingresa los datos de la venta
+# El usuario ingresa los datos de la venta
 while opcion == "si":
     Cliente = input("Nombre del cliente: ")
     Producto = input("producto: ")
     Costo = float(input("costo ($0.00): "))
 
-# Los datos ingresados se guardan en un diccionario    
+    # Los datos ingresados se guardan en un diccionario
     Registro = dict(Cliente=Cliente, Producto=Producto, Costo=Costo)
 
-# los datos del diccionario son ingresados en una lista    
+    # los datos del diccionario son ingresados en una lista
     RegistroVentas.append(Registro)
 
-# Se le pregunta si desea ingresar otra venta
+    # Se le pregunta si desea ingresar otra venta
     opcion = input("¿Deseas ingresar otro cliente?: ")
 
-# Se devuelven los datos ingresados    
+for Registro in RegistroVentas:
+    CostoTotal = CostoTotal + Registro.get("Costo")
+
+# Se devuelven los datos ingresados
 for Registro in RegistroVentas:
     print(Registro)
-CostosTotales = "Los costos totales obtenidos hasta el momento son: "
-for Registro in RegistroVentas:
-    print(Registro.get(Costo))
+
+print("El costo total es " + str(CostoTotal))
